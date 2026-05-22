9f6380ff7c44b71fb9ae6dd3b537096e08cc0c74
Alililaun56 <lilaunrapper@gmail.com>
2026-05-22 18:32:38 +0500

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFW62ReV0d7oU07iTd_UGfMqMly7iZMG8",
+  authDomain: "warisha-novels-world.firebaseapp.com",
+  projectId: "warisha-novels-world",
+  storageBucket: "warisha-novels-world.firebasestorage.app",
+  messagingSenderId: "730509359637",
+  appId: "1:730509359637:web:27a0e9ad2eb0196ed92c1c",
+  measurementId: "G-91RMFW4V74"
+};
